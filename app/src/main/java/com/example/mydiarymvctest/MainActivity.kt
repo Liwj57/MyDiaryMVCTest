7142e177bcd66b0e2bd23257895ae0b294442fcb
lewisli <lewisli@futunn.com>
2021-05-11 14:11:25 +0800

    first new branch

--- app/src/main/java/com/example/mydiarymvctest/MainActivity.kt
+++ app/src/main/java/com/example/mydiarymvctest/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mydiarymvctest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun select(search: String) {
+        Toast.makeText(this, search, Toast.LENGTH_SHORT).show()
     }
 }
